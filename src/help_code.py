479ebe5d89af08b753175b84f202cb6a2ecd3702
kimminwyk <57348147+kimminwyk@users.noreply.github.com>
2020-06-14 22:00:18 +0900

    file move
    file move

---
+++ src/help_code.py
@@ -0,0 +1,19 @@
+def help_code():    
+    print("""
+    <<< help >>>
+    userid userpw 만 입력할경우는 
+    attempt(id , pw)
+
+    file txt 로 하는경우는
+    txt = 
+    id:(you id)
+    pw:(you pw)
+    ↑
+    () 는 제거 공백없이
+    attelogin.file_name("filename" , "data") <- data 는 무슨 데이터가 들어가는지 알고싶을때만
+    예 :
+    {'lsd': 'lsd', 'charset_test': 'csettest', 'version': 'version', 'ajax': 'ajax', 'width': 'width', 'pxr': 'pxr', 'gps': 'gps', 'm_ts': 'mts', 'li': 'li',
+     'email': 'you id@email', 'pass': 'you pw', 'form': 'login'}
+    """)
+    
+help_code()
